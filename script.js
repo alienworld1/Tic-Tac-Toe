05911956b9585d3c03ee0f58263ae90bac9793bb
alienworld1 <astronomyguy0030@gmail.com>
2024-01-15 23:33:41 +0530

    Implement console version of tic tac toe

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const GameMaster = (function() {
         board: [['', '', ''], ['', '', ''], ['', '', '']],
 
         checkIfOccupied: (xPosition, yPosition) => {
-            if (this.board[yPosition][xPosition]) return true;
+            if (gameBoard.board[yPosition][xPosition]) return true;
             return false; 
         },
 
         placeSymbol: (symbol, xPosition, yPosition) => {
-            this.board[yPosition][xPosition] = symbol;
+            gameBoard.board[yPosition][xPosition] = symbol;
         },
 
         reset: () => {
-            this.board = [['', '', ''], ['', '', ''], ['', '', '']];
+            gameBoard.board = [['', '', ''], ['', '', ''], ['', '', '']];
         },
     };
 
@@ -26,30 +26,30 @@ const GameMaster = (function() {
             // and the winning player's symbol in case of a winner.
             
             for (let i = 0; i < 3; i++) {
-                
+
                 // Checking rows
                 if (gameBoard.board[i][0] === gameBoard.board[i][1] === gameBoard.board[i][2] && gameBoard.board[i][0]) {
                     return gameBoard.board[i][0];
                 }
 
                 // Checking columns
-                if (gameBoard.board[0][i] === gameBoard.board[1][i] === gameBoard.board[2][i] && gameBoard.board[0][i]) {
+                if (gameBoard.board[0][i] === gameBoard.board[1][i] && gameBoard.board[1][i] === gameBoard.board[2][i] && gameBoard.board[0][i]) {
                     return gameBoard.board[0][i];
                 }
             }
 
             // Checking diagonals
             if (gameBoard.board[1][1]) {
-                if (gameBoard.board[0][0] === gameBoard.board[1][1] === gameBoard.board[2][2]) return gameBoard.board[0][0];
-                if (gameBoard.board[0][2] === gameBoard.board[1][1] === gameBoard.board[2][0]) return gameBoard.board[0][2];
+                if (gameBoard.board[0][0] === gameBoard.board[1][1] && gameBoard.board[1][1] === gameBoard.board[2][2]) return gameBoard.board[0][0];
+                if (gameBoard.board[0][2] === gameBoard.board[1][1] && gameBoard.board[1][1] ===  gameBoard.board[2][0]) return gameBoard.board[0][2];
             }            
 
             // Checking if the board is full, (for a tie)
-            gameBoard.board.forEach((row) => {
-                row.forEach((cell) => {
+            for (const row of gameBoard.board) {
+                for (const cell of row) {
                     if (!cell) return 1;
-                });
-            });
+                }
+            }
 
             return 0;
             
@@ -79,12 +79,12 @@ const GameMaster = (function() {
     }
 
     function getCurrentTurn() {
-        if (gameIsOver()) return;
+        // if (gameIsOver()) return;
         return currentTurn;
     }
 
     function playMove(xPosition, yPosition) {
-        if (gameIsOver()) return;
+        // if (gameIsOver()) return;
 
         if (gameBoard.checkIfOccupied(xPosition, yPosition)) {
             return -1;
@@ -97,15 +97,21 @@ const GameMaster = (function() {
 
     const ConsoleController = {
         printBoard: () => {
-            console.log(gameBoard.board);
+            gameBoard.board.forEach((row) => {
+                console.log(row);
+            })
         },
     }
+
+    const getResult = () => gameController.evaluateBoard();
 
     return {
         initializePlayers,
         getCurrentTurn,
         playMove,
         ConsoleController,
+        gameIsOver,
+        getResult,
     }
 
 })();
@@ -127,3 +133,20 @@ const player2Name = prompt("Enter the second player's name") ?? "player2";
 
 const player1 = Player(player1Name, true);
 const player2 = Player(player2Name, false);
+
+GameMaster.initializePlayers(player1, player2);
+
+while (!GameMaster.gameIsOver()) {
+    currentPlayer = GameMaster.getCurrentTurn();
+    console.log(`It is ${currentPlayer.name}'s turn!`);
+    let x = +prompt("Type the x position to place your marker.");
+    let y = +prompt("Type the y position to place your marker.");
+    GameMaster.playMove(x, y);
+    GameMaster.ConsoleController.printBoard();
+}
+
+let result = GameMaster.getResult();
+if (result === "X") console.log(`${player1.name} wins!`);
+else if (result === "O") console.log(`${player2.name} wins!`);
+else console.log("It's a tie!");
+
